b12dab422553d48a977bf06fe8fb45be2f48a97f
Leah Dudley <leahtsdudley@gmail.com>
2021-01-15 09:41:19 -0500

    Export assets works

--- src/export-assets.js
+++ src/export-assets.js
@@ -1,6 +1,34 @@
 import sketch from 'sketch'
-// documentation: https://developer.sketchapp.com/reference/api/
+// import output from "./classes/output";
+var document = require('sketch/dom').getSelectedDocument();
 
-export default function() {
-  sketch.UI.message("It's alive 🙌")
+export default function () {
+  var pages = document.pages;
+  pages.forEach(function (page) {
+    var assets = page.layers.filter(function (sel) {
+      return ['SymbolMaster', 'Artboard'].includes(sel.type) && sel.exportFormats.length > 0;
+    });
+    var options = {
+      output: outputDir(),
+      scales: '1',
+      formats: 'png',
+      overwriting: true
+    };
+    assets.forEach(function (layer) {
+      if (layer.exportFormats.length === 2) {
+        options.scales = '1, 2'
+      } else {
+        options.scales = '1'
+      }
+      sketch.export(layer, options);
+    });
+  });
+  sketch.UI.message('Assets Exported!');
+}
+
+function outputDir() {
+  var path = require('path');
+  // documentation: https://developer.sketchapp.com/reference/api/
+  var outputDirectory = path.parse(document.path).dir + "/Assets/";
+  return outputDirectory.split("%20").join(" ");
 }
